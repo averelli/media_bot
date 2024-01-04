7f95dbf2b19fe079d4bf8f479d362d2f68d4e538
Averelli <beloglazov.roman06@gmail.com>
2024-01-04 21:37:39 +0300

    Add variables to work with Google sheets

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,13 @@ load_dotenv()
 
 # define the env vars
 TELEGRAM_BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+# for the database
 DB_NAME = os.environ.get("DB_NAME")
 USER = os.environ.get("USER") 
 PASSWORD = os.environ.get("PASSWORD")
 HOST = os.environ.get("HOST")
 PORT = os.environ.get("PORT")
+
+# name of the google sheets file that stores offline data
+GS_FILE = os.environ.get("GS_FILE")
