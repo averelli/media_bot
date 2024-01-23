d3e2b982544294ae8b57cab77418f93c58917965
Averelli <beloglazov.roman06@gmail.com>
2024-01-23 18:08:18 +0300

    Rework and finalize creation of the plots for books and shows

--- plots.py
+++ plots.py
@@ -5,8 +5,27 @@ from matplotlib.colors import rgb2hex
 from connect_db import send_query
 import pandas as pd
 import numpy as np
+import io
 
-def plot_report(type, date_start):
+# define figure and buffer so they can be closed from another function
+image_buffer = None
+fig = None
+
+def plot_report(type: str, date_start):
+    """
+    Returns a plot of data for the specific date range given the start date
+
+    Args:
+        type (str): takes either reading or shows
+        date_start (datetime): takes a datetime object to specify the start of the range
+
+    Returns:
+        (None): Creates a plot
+    """
+
+    # get global vars
+    global image_buffer, fig
+    
     # get data for the plot based on the type param
     if type == "reading":
         df = pd.DataFrame(send_query("""
@@ -43,7 +62,6 @@ def plot_report(type, date_start):
 
     # Plot the data
     # if there are a lot of titles, there will be no legend and no colors for the bars
-    print('len is ', len(unique_labels))
     if len(unique_labels) > 20:
         ax.bar(df["date"], df["value"],  zorder=2)
     else:
@@ -53,7 +71,6 @@ def plot_report(type, date_start):
 
         # add a color column to the df
         df["color"] = df["title"].map(label_colors)
-        print(len(colors))
 
         ax.bar(df["date"], df["value"], color=df["color"],  zorder=2)
 
@@ -82,7 +99,6 @@ def plot_report(type, date_start):
     y_ticks = np.arange(0, y_max + 1, y_max / 4)
     plt.yticks(y_ticks)
 
-
     # add a grid 
     ax.grid(axis='x', linestyle='--', alpha=0)
     ax.grid(axis='y', linestyle='--', alpha=0.7, zorder=1)
@@ -91,6 +107,7 @@ def plot_report(type, date_start):
     fig.patch.set_facecolor('#262735')
     ax.set_facecolor("#323447")
 
+    # set colors for the plot border and ticks
     ax.spines['bottom'].set_color("#f8faf7")
     ax.spines['top'].set_color("#f8faf7") 
     ax.spines['right'].set_color("#f8faf7")
@@ -101,11 +118,26 @@ def plot_report(type, date_start):
     plt.xlabel("Date", color="#f8faf7")
 
     label_param = "Words" if type == "reading" else "Minutes watched"
-
     plt.ylabel(label_param, color="#f8faf7")
     plt.title(f"{label_param} read over time", color="#f8faf7")
 
-    
+    # create a buffer to store the plot as an image
+    image_buffer = io.BytesIO()
+    plt.savefig(image_buffer, format="png")
+    image_buffer.seek(0)
 
-    # Show the plot
-    plt.show()
+    return image_buffer
+
+
+def close_plots():
+    """Close the fig and buffer used for the plots"""
+
+    global image_buffer, fig
+
+    if image_buffer and not image_buffer.closed:
+        image_buffer.close()
+        buffer = None
+    
+    if fig:
+        plt.close(fig)
+        fig = None
